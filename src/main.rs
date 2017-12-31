916aa7ccf04a3ff3d27144b7afc30894fcdd1c5d
tickelton <tickelton@gmail.com>
2017-12-31 18:39:15 +0100

    Added logging using env_logger.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 use clap::{Arg, App};
 
 fn main() {
+    match env_logger::init() {
+        Ok(_) => trace!("env_logger successfully initialized"),
+        Err(e) => eprintln!("WARNING: unable to initialize env_logger: {}", e),
+    }
+
     let matches = App::new("flopbear")
                         .version("0.1.0")
                         .author("<tickelton@gmail.com>")
@@ -12,8 +20,7 @@ fn main() {
                              .required(true)
                              .index(1))
                         .get_matches();
-
-    println!("Using interface: {}", matches.value_of("INTERFACE").unwrap());
-
+    let iface = matches.value_of("INTERFACE").unwrap();
+    debug!("Using interface: {}", iface);
 }
 
