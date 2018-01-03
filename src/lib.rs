705356f5390029e2927e22a35826023f095553c3
tickelton <tickelton@gmail.com>
2018-01-03 18:57:00 +0100

    Config::new() will now find the correct network interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,61 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate get_if_addrs;
+extern crate clap;
+
+use clap::{Arg, App};
+use get_if_addrs::IfAddr::V4;
+
+#[derive(Debug)]
+pub struct Config {
+    pub addr: get_if_addrs::Ifv4Addr,
+}
+
+impl Config {
+    pub fn new() -> Result<Config, &'static str> {
+        let matches = App::new("flopbear")
+                            .version("0.1.0")
+                            .author("<tickelton@gmail.com>")
+                            .about("A configuration-less DHCP Server")
+                            .arg(Arg::with_name("INTERFACE")
+                                 .help("Network interface to listen on")
+                                 .required(true)
+                                 .index(1))
+                            .get_matches();
+        let if_name = matches.value_of("INTERFACE").unwrap();
+        debug!("Config: using interface: {}", if_name);
+
+        let interfaces: Option<get_if_addrs::Ifv4Addr> = match get_if_addrs::get_if_addrs() {
+            Ok(i) => {
+                match i.iter()
+                        .filter(|iface| { iface.name == if_name})
+                        .filter(|iface2| {
+                            match iface2.addr {
+                                V4(_) => true,
+                                _ => false
+                                }
+                            })
+                        .next() {
+
+                    Some(x) => match x.addr.clone() {
+                        V4(y) => Some(y),
+                        _ => None,
+                    },
+                    None => None
+                }
+            },
+            Err(e) => { eprintln!("Error: {}", e); None } ,
+        };
+        debug!("Config: interfaces = {:?}", interfaces);
+
+        match interfaces {
+            Some(addr) => Ok(Config { addr }),
+            None => Err("Error: interface not found"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
-extern crate clap;
+extern crate flopbear;
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate get_if_addrs;
 
-use clap::{Arg, App};
+use std::process;
+use flopbear::Config;
 
 fn main() {
     match env_logger::init() {
         Ok(_) => trace!("env_logger successfully initialized"),
         Err(e) => eprintln!("WARNING: unable to initialize env_logger: {}", e),
-    }
+    };
 
-    let matches = App::new("flopbear")
-                        .version("0.1.0")
-                        .author("<tickelton@gmail.com>")
-                        .about("A configuration-less DHCP Server")
-                        .arg(Arg::with_name("INTERFACE")
-                             .help("Network interface to listen on")
-                             .required(true)
-                             .index(1))
-                        .get_matches();
-    let if_name = matches.value_of("INTERFACE").unwrap();
-    debug!("Using interface: {}", if_name);
+	let config = Config::new().unwrap_or_else(|err| {
+        error!("{}", err);
+        process::exit(1);
+    });
 
-	let interfaces = match get_if_addrs::get_if_addrs() {
-		Ok(i) => i,
-		Err(e) => { eprintln!("Error: {}", e); std::vec::Vec::new() } ,
-	};
-	println!("interfaces = {:?}", interfaces);
-
+    println!("config={:?}", config);
 }
 
