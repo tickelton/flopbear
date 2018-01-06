e13a7c1b0968e2aa6258572f8035fce04952b988
tickelton <tickelton@gmail.com>
2018-01-06 18:02:56 +0100

    Using argp for argument parsing.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include "flopbear.h"
 
-int main(int argc, char **argv)
+error_t
+parse_opt (int key, char *arg, struct argp_state *state)
 {
-	printf("Hello, World!\n");
-	return 0;
+    struct arguments *arguments = state->input;
+
+    switch (key) {
+      case ARGP_KEY_ARG:
+        if (state->arg_num >= 1) {
+            argp_usage(state);
+        }
+        arguments->ifname = arg;
+        break;
+      case ARGP_KEY_END:
+        if (state->arg_num < 1) {
+            argp_usage (state);
+        }
+        break;
+      default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+    struct arguments arguments;
+
+    if(argp_parse(&argp, argc, argv, 0, 0, &arguments)) {
+        // TODO: this should never happen. argp_parse exits
+        //       on error. Could be handled better.
+        fprintf(stderr, "Error parsing arguments\n");
+        return 1;
+    }
+
+    printf("ifname=%s\n", arguments.ifname);
+
+    return 0;
 }
